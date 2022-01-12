a1b2137b7df6a7961c27b7c18756f01f54f53e64
HelgaNaz <onworkit@gmail.com>
2022-01-12 13:23:19 +0200

    API-CORS

---
+++ src/js/01-fetch-api.js
@@ -0,0 +1,48 @@
+import '../css/common.css';
+import pokemonCardTpl from '../templates/pokemon-card.hbs';
+
+// ======================================
+import API from './api-service';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  console.log(form)
+  // query это name нашего input
+  const searchQuery = form.elements.query.value;
+  
+  console.log(form.elements.query)
+
+  API.fetchPokemon(searchQuery)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+}
+
+// =========================================
+
+const url = 'https://newsapi.org/v2/everything?q=cars';
+const options = {
+  headers: {
+    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+  },
+};
+
+fetch(url, options)
+  .then(r => r.json())
+  .then(console.log);
