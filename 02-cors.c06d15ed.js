0b4f09bb409221288c764369a15bbe5ced462ab6
helganaz <87029353+helganaz@users.noreply.github.com>
2022-01-12 15:06:14 +0000

    Deploying to gh-pages from @ helganaz/goit-js-api@0ef39c8066a1571b93006f1b3d2cc06d59f5aa35 🚀

--- 02-cors.c06d15ed.js
+++ 02-cors.c06d15ed.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Jaai":[function(require,module,exports) {
 "use strict";require("../css/common.css"),fetch("https://lpj-weather-service.herokuapp.com/weather?lat=39.916668&lon=116.383331").then(e=>e.json()).then(console.log);
 },{"../css/common.css":"en75"}]},{},["Jaai"], null)
-//# sourceMappingURL=/goit-js-api/02-cors.c53429a1.js.map
+//# sourceMappingURL=/goit-js-api/02-cors.c06d15ed.js.map
